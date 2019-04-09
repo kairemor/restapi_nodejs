ebcb5f881827612234706bda4ac368b5bcfe927a
mor kaire <kairemor@gmail.com>
2019-04-09 11:31:54 +0000

    basic authentication

--- index.js
+++ index.js
@@ -14,6 +14,36 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
+function auth(req, res, next){
+    console.log(req.headers); 
+
+    var authorization = req.headers.authorization ; 
+
+    if (!authorization){
+        var error = new Error("You're not authenticate guys "); 
+        res.setHeader("WWW-Authenticate", "Basic"); 
+        res.statusCode = 401 ; 
+        return next(error); 
+    }
+
+    var auth = new Buffer(authorization.split(' ')[1], 'base64').toString().split(':'); 
+
+    var username = auth[0];
+    var passwd = auth[1];
+
+    if (username === 'admin' && passwd === 'password'){
+        return next() ; 
+    }else{
+        var error = new Error("You're not authenticate guys "); 
+        res.setHeader("WWW-Authenticate", "Basic"); 
+        res.statusCode = 401 ; 
+        return next(error);            
+    }
+
+} 
+
+app.use(auth); 
+app.use(express.static(__dirname+'/public'));
 app.use('/dishes', DishRouter) 
 app.use('/promotions', PromotionRouter)
 app.use('/leaders', LeaderRouter)
