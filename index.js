437646ebf3bd8130c54c18c85a4cfdb77a603fff
mor kaire <kairemor@gmail.com>
2019-04-11 13:38:31 +0000

    session adding

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const morgan  = require('morgan')
 const http = require('http')
+const session = require('express-session')
+const fileStore = require('session-file-store').session();
 
 const DishRouter = require('./routes/DishRouter')
 const LeaderRouter = require('./routes/leaderRouter')
@@ -14,13 +16,59 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(cookieParser('kairemor-12345'))
+// app.use(cookieParser('kairemor-12345'))
+app.use(session({
+    name : 'session_id',
+    secret:'kairemor-12345',
+    saveUninitialized: false,
+    resave: false , 
+    store : new fileStore()
+}))
 
+// function auth(req, res, next){
+//     console.log(req.headers); 
+//     console.log('Cookie header ', req.cookie)
+
+//     if (!req.signedCookies.user){
+//         var authorization = req.headers.authorization ; 
+
+//         if (!authorization){
+//             var error = new Error("You're not authenticate guys "); 
+//             res.setHeader("WWW-Authenticate", "Basic"); 
+//             res.statusCode = 401 ; 
+//             return next(error); 
+//         }
+    
+//         var auth = new Buffer.from(authorization.split(' ')[1], 'base64').toString().split(':'); 
+    
+//         var username = auth[0];
+//         var passwd = auth[1];
+    
+//         if (username === 'admin' && passwd === 'password'){
+//             res.cookie('user', 'admin', {signed:true}) ;
+//             return next() ; 
+//         }else{
+//             var error = new Error("You're not authenticate guys "); 
+//             res.setHeader("WWW-Authenticate", "Basic"); 
+//             res.statusCode = 401 ; 
+//             return next(error);            
+//         }
+//     }else{
+//         if(req.signedCookies.user === 'admin'){
+//             return next();
+//         }else{
+//             var error = new Error("You're not authenticate guys "); 
+//             res.setHeader("WWW-Authenticate", "Basic"); 
+//             res.statusCode = 401 ; 
+//             return next(error);   
+//         }
+//     }
+// } 
 function auth(req, res, next){
     console.log(req.headers); 
-    console.log('Cookie header ', req.cookie)
+    console.log('session header ', req.sessio)
 
-    if (!req.signedCookies.user){
+    if (!req.session.user){
         var authorization = req.headers.authorization ; 
 
         if (!authorization){
@@ -36,7 +84,7 @@ function auth(req, res, next){
         var passwd = auth[1];
     
         if (username === 'admin' && passwd === 'password'){
-            res.cookie('user', 'admin', {signed:true}) ;
+            req.session.user = 'admin' ;
             return next() ; 
         }else{
             var error = new Error("You're not authenticate guys "); 
@@ -45,7 +93,7 @@ function auth(req, res, next){
             return next(error);            
         }
     }else{
-        if(req.signedCookies.user === 'admin'){
+        if(req.session.user === 'admin'){
             return next();
         }else{
             var error = new Error("You're not authenticate guys "); 
@@ -54,9 +102,6 @@ function auth(req, res, next){
             return next(error);   
         }
     }
-
-
-
 } 
 
 app.use(auth); 
